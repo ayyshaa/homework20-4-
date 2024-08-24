6a3a1acb1d17869df340901b05b96a5921ac7e27
ayyshaa <qacarovaayse@gmail.com>
2024-08-24 18:05:25 +0400

    work4

---
+++ app.js
@@ -0,0 +1,11 @@
+let text = prompt("String ifade daxil edin");
+console.log(text.length);
+
+
+console.log(text.includes("e"));
+
+console.log(text.startsWith("code"));
+
+console.log(text.endsWith("code"));
+
+console.log(text.replaceAll("t","####"));
